8d2482c9c9699de63a2d6af98149453c41dc6665
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-04-09 19:07:13 +0300

    Update iftarsahur_bot.py

--- iftarsahur_bot.py
+++ iftarsahur_bot.py
@@ -133,7 +133,7 @@ def restart(client, message: Message):
     else:
         toSendStr = "🇹🇷 Yeniden Başlatıldı"
         toSendStr += "\n🇬🇧 Restarted"
-        sendMessage(message, toSendStr)
+        send_message(message, toSendStr)
 
 @app.on_message(f.command('music'))
 async def a(client, message):
